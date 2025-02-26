08fc1f7c603edf6f8dbeb8fb31f46cec35de8df1
RohitBhojak <rohitbhojak99@gmail.com>
2025-02-26 14:13:05 +0000

    Complete home tab

--- src/home.js
+++ src/home.js
@@ -1,19 +1,18 @@
-export function loadHome() {
+export default function loadHome() {
     const content = document.querySelector('#content');
+    content.classList.remove(...content.classList);
+    content.classList.add("home");
     content.innerHTML = "";
 
     const container = document.createElement('div');
+    container.id = "home";
     const title = document.createElement('h1');
     title.textContent = "HALDII";
 
     const type = document.createElement('h5');
     type.textContent = "Restaurant + Cafe";
 
-    const tagLine = document.createElement('h3');
-    tagLine.textContent = "A PURE VEG RESTAURANT";
-
     container.appendChild(title);
     container.appendChild(type);
-    container.appendChild(tagLine);
     content.appendChild(container);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import { loadHome } from "./home";
+import loadHome from "./home";
 import "./styles.css";
+import "./home-style.css";
 
 document.addEventListener('DOMContentLoaded', loadHome);
+
+const homeButton = document.querySelector('#home');
+const menuButton = document.querySelector('#home');
+const aboutButton = document.querySelector('#home');
+
+homeButton.addEventListener('click', loadHome);
