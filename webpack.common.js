13987d3632f5c67ea45e64f3f98bfff2d9970315
RohitBhojak <rohitbhojak99@gmail.com>
2025-03-02 15:47:55 +0000

    Fix missing webpack config

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        clean: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
